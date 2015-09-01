68d22678deddcdf5c7bfc6f2866fa5e3a739bc0e
jaredgeizer <jaredg@Jareds-MacBook-Pro.local>
2015-09-01 15:44:34 -0400

    first commit
    trying to find where the commas go, i started by copying the presidents
    example, i think i have a fair grasp on these basics

---
+++ js/players.js
@@ -0,0 +1,387 @@
+
+
+var players = [
+    {
+        name: { 
+            first: 'Devion', 
+            last: 'Whetstone' 
+        }, 
+        number: 1, 
+        yearStudent: 'Senior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 3
+            },
+            weight: {
+                pounds: 200,
+            },
+        },
+        position: 'MB',
+        Hometown: {
+            town: 'Missouri City',
+            state: 'Texas',
+            country: 'USA'
+        },
+        previousSchool: 'The Village School',
+    }, 
+    {
+        name: { 
+            first: 'Andrew', 
+            last: 'Washam' 
+        }, 
+        number: 2, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 2
+            },
+            weight: {
+                pounds: 160,
+            },
+        },
+        position: 'OH',
+        Hometown: {
+            town: 'Boca Raton',
+            state: 'Florida',
+            country: 'USA'
+        },
+        previousSchool: 'Boca Raton High School',
+    }, 
+    {
+        name: { 
+            first: 'Gary', 
+            last: 'Baker' 
+        }, 
+        number: 3, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 5,
+                inches: 10
+            },
+            weight: {
+                pounds: 143,
+            },
+        },
+        position: 'L',
+        Hometown: {
+            town: 'Altamonte Springs',
+            state: 'Florida',
+            country: 'USA'
+        },
+        previousSchool: 'Lyman Hgh School',
+    }, 
+    {
+        name: { 
+            first: 'Gabriel', 
+            last: 'Castro' 
+        }, 
+        number: 5, 
+        yearStudent: 'Freshman',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 3
+            },
+            weight: {
+                pounds: 175,
+            },
+        },
+        position: 'OH',
+        Hometown: {
+            town: 'Belo Horizonte',
+            state: '',
+            country: 'Brazil'
+        },
+        previousSchool: 'Colegio Rui Barbosa',
+    }, 
+    {
+        name: { 
+            first: 'Jesse', 
+            last: 'Day' 
+        }, 
+        number: 6, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 5,
+                inches: 11
+            },
+            weight: {
+                pounds: 145,
+            },
+        },
+        position: 'L',
+        Hometown: {
+            town: 'Winter Park',
+            state: 'Florida',
+            country: 'USA'
+        },
+        previousSchool: 'Colegio Rui Barbosa',
+    }, 
+    {
+        name: { 
+            first: 'Luke', 
+            last: 'Grupe' 
+        }, 
+        number: 7, 
+        yearStudent: 'Senior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 6
+            },
+            weight: {
+                pounds: 170,
+            },
+        },
+        position: 'MB',
+        Hometown: {
+            town: 'Lexington',
+            state: 'Kentucky',
+            country: 'USA'
+        },
+        previousSchool: 'Henry Clay High School',
+    }, 
+    {
+        name: { 
+            first: 'Colton', 
+            last: 'Parsons' 
+        }, 
+        number: 68, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 4
+            },
+            weight: {
+                pounds: 155,
+            },
+        },
+        position: 'S',
+        Hometown: {
+            town: 'Neenah',
+            state: 'Wisconson',
+            country: 'USA'
+        },
+        previousSchool: 'Neenah High School',
+    }, 
+    {
+        name: { 
+            first: 'Liam', 
+            last: 'Flaherty' 
+        }, 
+        number: 9, 
+        yearStudent: 'Senior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 1
+            },
+            weight: {
+                pounds: 190,
+            },
+        },
+        position: 'L',
+        Hometown: {
+            town: 'West Chicago',
+            state: 'Illanois',
+            country: 'USA'
+        },
+        previousSchool: 'West Chicago Community High School',
+    }, 
+    {
+        name: { 
+            first: 'Leshon', 
+            last: 'Alexander' 
+        }, 
+        number: 10, 
+        yearStudent: 'Senior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 4
+            },
+            weight: {
+                pounds: 165,
+            },
+        },
+        position: 'MB',
+        Hometown: {
+            town: 'Methuen',
+            state: 'Massachusetts',
+            country: 'USA'
+        },
+        previousSchool: 'Barbados CC',
+    }, 
+    {
+        name: { 
+            first: 'Luke', 
+            last: 'Hansen' 
+        }, 
+        number: 12, 
+        yearStudent: 'Senior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 1
+            },
+            weight: {
+                pounds: 175,
+            },
+        },
+        position: 'OH',
+        Hometown: {
+            town: 'Las Vegas',
+            state: 'Nevada',
+            country: 'USA'
+        },
+        previousSchool: 'Las Vegas High School',
+    }, 
+    {
+        name: { 
+            first: 'Artur', 
+            last: 'Cabral' 
+        }, 
+        number: 13, 
+        yearStudent: 'Junior',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 5
+            },
+            weight: {
+                pounds: 180,
+            },
+        },
+        position: 'OH',
+        Hometown: {
+            town: 'Maceio',
+            state: '',
+            country: 'Brazil'
+        },
+        previousSchool: 'Colegio Pitagroas Cidade Jardim',
+    }, 
+    {
+        name: { 
+            first: 'Josiah', 
+            last: 'Douglas' 
+        }, 
+        number: 14, 
+        yearStudent: 'Freshman',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 4
+            },
+            weight: {
+                pounds: 170,
+            },
+        },
+        position: 'OH',
+        Hometown: {
+            town: 'Chicopee',
+            state: 'Massachusetts',
+            country: 'USA'
+        },
+        previousSchool: 'Chicopee Comprehensive High School',
+    }, 
+    {
+        name: { 
+            first: 'Joao', 
+            last: 'Victor Santos' 
+        }, 
+        number: 15, 
+        yearStudent: 'Freshman',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 7
+            },
+            weight: {
+                pounds: 202,
+            },
+        },
+        position: 'MB',
+        Hometown: {
+            town: 'Brasilia',
+            state: '',
+            country: 'Brazil'
+        },
+        previousSchool: 'Centro Educational Brasil Futuro',
+    }, 
+    {
+        name: { 
+            first: 'Aleksa', 
+            last: 'Bijelic' 
+        }, 
+        number: 16, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 1
+            },
+            weight: {
+                pounds: 158,
+            },
+        },
+        position: 'S',
+        Hometown: {
+            town: 'Subotica',
+            state: '',
+            country: 'Serbia'
+        },
+        previousSchool: 'Politechnical High School',
+    }, 
+    {
+        name: { 
+            first: 'James', 
+            last: 'Fulton' 
+        }, 
+        number: 17, 
+        yearStudent: 'Sophomore',
+        bioInfo: {
+            height: {
+                feet: 6,
+                inches: 2
+            },
+            weight: {
+                pounds: 175,
+            },
+        },
+        position: 'MB',
+        Hometown: {
+            town: 'Baldwin',
+            state: 'New York',
+            country: 'USA'
+        },
+        previousSchool: 'Holy Trinity High School',
+    }, 
+    
+];
+
+var totalInches = ( players[0].height.feet * 12 ) + players[0].height.inches;
+alert( players[0].name.first + ' ' + players[0].name.last + ' is ' + totalInches + ' inches tall.' );
+
+
+/*
+name
+    first
+    last
+number
+year
+height
+    feet
+    inches
+weight
+    pounds
+position
+hometown
+    town
+    state
+    country
+previous school
